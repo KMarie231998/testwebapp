65a79b309c1989a1d91df847faf7b5eee1000416
KMarie231998 <66622944+KMarie231998@users.noreply.github.com>
2020-06-10 12:21:45 +0800

    Update viewrest.php

--- viewrest.php
+++ viewrest.php
@@ -1,1 +1,29 @@
+<?php
+  // SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "Km2502275", "Database" => "WebApplicationDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:webapptp046942.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+if(!$conn){
+  die("Error connection failed:".sqlsrv_errors());
+}
+echo "Connection to DB:Success!";
 
+$sql=" SELECT * FROM [dbo].[restaurant]";
+$getResults = sqlrsv_query($conn, $sql);
+  if ($getResults == FALSE)
+  {
+    die(sqlsrv_errors());
+  }
+  echo "<table border = '1'>";
+  while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+  {
+  echo "<tr>";
+  echo "<td>". $row['restaurant_id'] . "</td>";
+  echo "<td>". $row['restaurant_name'] ."</td>";
+  echo "<td>". $row['restaurant_address'] . "</td>";
+  echo "<td>". $row['restaurant_phone'] . "</td>";
+  echo "</tr>";
+  }
+  echo "</table>";
+  sqlsrv_free_stmt($getResults);
+?>
